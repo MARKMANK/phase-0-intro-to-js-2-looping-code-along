05c265de5f6f7c0dfa1e6911b400a8083a965f7a
kevinmarkman <kevin.w.markman@gmail.com>
2023-01-27 19:37:58 -0600

    Target runs

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age}years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!]`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+
+
+
+function writeCards(nameOfRecipient, ocassion) {
+    const returnedArray = [];
+
+    for (let c = 0; c <nameOfRecipient.length; c++) {
+        const card = (`Thank you, ${nameOfRecipient[c]}, for the wonderful surprise gift!`);
+        returnedArray.push(card);
+    }
+    return returnedArray;
+}
+
+function countDown(integer){
+    for (let i = integer; i >= 0; i--) {
+        console.log(i);
+    }
+    return integer;
+}
